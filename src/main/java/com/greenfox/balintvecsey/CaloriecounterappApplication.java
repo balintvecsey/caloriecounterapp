782517fb5958ae2eb4af1d3a059d08055351e4c0
balintvecsey <balint.vecsey@gmail.com>
2017-06-02 09:43:31 +0200

    create new project, add .gitignore

---
+++ src/main/java/com/greenfox/balintvecsey/CaloriecounterappApplication.java
@@ -0,0 +1,12 @@
+package com.greenfox.balintvecsey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaloriecounterappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaloriecounterappApplication.class, args);
+	}
+}
---
+++ src/test/java/com/greenfox/balintvecsey/CaloriecounterappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.greenfox.balintvecsey;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CaloriecounterappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
